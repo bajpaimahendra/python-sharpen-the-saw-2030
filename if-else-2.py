4e4b8f9e7e72c55e38d08761d7d18d91ad21e863
Mahendra <mbajpai@contecglobal.com>
2026-03-30 16:36:05 +0530

    ok

--- if-else-2.py
+++ if-else-2.py
@@ -18,8 +18,69 @@ else:
 
 
 print('------- Example-2 --------------------------')
-password = '1234'
-if '1234'== password:
-    print('')
+password = '1234'   
+if '1234'== password:   # == means compare values
+    print('Login Successful')
+else:
+    print('Wrong Password')
+
+
+print('------- Example-3 Even or Odd--------------------------')
+number =7
+if number % 2 ==0:  # % = remainder
+    print('Even Number')
+else:
+    print('Odd Number')
+
+
+print('------- Example-4  elif Multiple Conditions ----------------------')
+marks = 75
+if marks >= 90:
+    print('Grade A')
+elif marks >= 60:
+    print('Grade B')
+else:
+    print('Failed')
+
+
+
+
+print('------- Example-5  Taking Input ----------------------')
+age = input('Enter your age :')
+age = int(age)
+if age >= 18:
+    print('Adult')
+else:
+    print('Minor')
+
+
+print('------- Example-6  Nested if  ----------------------')
+age = 12
+has_id = True
+if age >= 18:
+    print('Adult')
+    if has_id:
+        print('Entry Allowed')
+    else:
+        print('ID requried')
+else:
+    print('Underage')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
---
+++ operators-3.py
@@ -0,0 +1,20 @@
+################## Comparison Operators (Most Used) #############################################
+#           
+#        | Operator | Meaning          |
+#        | -------- | ---------------- |
+#        | `==`     | equal            |
+#        | `!=`     | not equal        |
+#        | `>`      | greater          |
+#        | `<`      | smaller          |
+#        | `>=`     | greater or equal |
+#        | `<=`     | smaller or equal |
+#
+################# Logical Operators (Combine Conditions) ##########################################
+#
+#       | Operator | Meaning           |
+#       | -------- | ----------------- |
+#       | `and`    | both must be True |
+#       | `or`     | any one True      |
+#       | `not`    | reverse           |
+#
+####################################################################################################
