40d263735bc980ca435a28aa393a937b5f9449a1
Mahendra <mbajpai@contecglobal.com>
2026-03-30 16:00:07 +0530

    ok

---
+++ if-else-2.py
@@ -0,0 +1,25 @@
+################################################################
+#       👉 Clean indentation = important (no {} like PHP)
+#
+#        if condition:
+#            # runs if condition is True
+#        else:
+#            # runs if condition is False
+#
+#
+################################################################
+
+print('------- Example-1 --------------------------')
+age = 18
+if age >= 18:
+    print('You can vote')
+else:
+    print('You can not vote')
+
+
+print('------- Example-2 --------------------------')
+password = '1234'
+if '1234'== password:
+    print('')
+
+
